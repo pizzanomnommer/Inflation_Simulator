054f8789cbc1bc13e08c64839f7a279f0a3cfad7
pizzanomnommer <46574388+pizzanomnommer@users.noreply.github.com>
2019-02-28 02:21:36 -0800

    Update API.js

--- API.js
+++ API.js
@@ -2,4 +2,5 @@ $.getJSON('https://api.transparency.treasury.gov/services/api/fiscal_service/v1/
     console.log(data);
 
     var CalYear = "https://api.transparency.treasury.gov/services/api/fiscal_service/v1/accounting/od/debt_to_penny?sort=-data_date&page%5Bnumber%5D=1&page%5Bsize%5D=1" + data.reporting_calendar_year;
+    console.log(CalYear);
 });
